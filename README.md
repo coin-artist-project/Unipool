ee34ac55ca12e577548715e590bdb1ad82543922
Cybourgeoisie <rbenheidorn@gmail.com>
2020-10-22 05:44:45 -0400

    Changes to suit my development environment and style

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Unipool
 
-Staking pool for Uniswap ETH/sETH liquidity token for receiving 72000 SNX weekly reward
-
-[![Build Status](https://travis-ci.org/k06a/Unipool.svg?branch=master)](https://travis-ci.org/k06a/Unipool)
-[![Coverage Status](https://coveralls.io/repos/github/k06a/Unipool/badge.svg?branch=master)](https://coveralls.io/github/k06a/Unipool?branch=master)
+Staking pool for Uniswap ETH/{COIN} liquidity token for receiving regular rewards
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const infura_key = fs.readFileSync(__dirname + "/.infura").toString().trim();
+const rinkeby_private_key = fs.readFileSync(__dirname + "/.secret.rinkeby.key").toString().trim();
+const mainnet_private_key = fs.readFileSync(__dirname + "/.secret.mainnet.key").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -15,6 +22,24 @@ module.exports = {
       network_id: '*',
       gas: 8000000,
       gasPrice: 1000000000, // web3.eth.gasPrice
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(
+        rinkeby_private_key,
+        'https://rinkeby.infura.io/v3/' + infura_key
+      ),
+      gas: 6721975,
+      gasPrice: 80000000000, // 80 gwei
+      network_id: '4'
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(
+        mainnet_private_key,
+        'https://mainnet.infura.io/v3/' + infura_key
+      ),
+      gas: 6721975,
+      gasPrice: 80000000000, // 80 gwei
+      network_id: '1'
     }
   },
   compilers: {
